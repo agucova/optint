169c063ead6108fc8377b8702c2cb019a72c6079
IgnAMS <nanomonardes123@gmail.com>
2021-06-30 16:12:46 -0400

    modelo de las camas

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ from typing import List
 
 from gurobipy import GRB, Model, quicksum, Env
 
-from metrics import metrics
+from metrics import metrics 
 from paciente import Paciente
 from parameters import gen_patients
 
 
-def optimize_beds(n_beds: int, n_patients: int, cost: List[int]) -> dict:
+def optimize_beds(n_beds: int, n_patients: int, cost: List[int], A=None) -> dict:
     """Defines and optimizes the full bed distribution model.
     Returns whether it's feasible, the number of non-ideal beds, the number of changed beds and the total distance."""
 
@@ -56,8 +56,8 @@ def optimize_beds(n_beds: int, n_patients: int, cost: List[int]) -> dict:
     ]  # matriz
 
     P, G, I, E_start, E_end, V, S = gen_patients(n_patients)
-
-    A = [0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 1, 1]
+    if A is None:
+        A = [0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 1, 1]
     Q = 7
 
     # Tipo de cama
